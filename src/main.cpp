b5cac4b252ab4e59062fa4264954192b80185262
gianmarco-mameli <gnammyx@gmail.com>
2023-04-03 21:10:38 +0200

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,29 @@ const char *mqtt_broker = "192.168.1.1";
 const int mqtt_port = 1883;
 
 #ifdef LEFT
-  const char *client_id = "rackfanleft";
-  const char *fan1_status_topic = "rackfanleft/fan1/status";
-  const char *fan2_status_topic = "rackfanleft/fan2/status";
-  const char *fan1_topic = "rackfanleft/fan1/state";
-  const char *fan2_topic = "rackfanleft/fan2/state";
+const char *client_id = "rackfanleft";
+const char *status_topic = "rackfanleft/status";
+const char *fan1_status_topic = "rackfanleft/fan1/status";
+const char *fan2_status_topic = "rackfanleft/fan2/status";
+const char *fan1_topic = "rackfanleft/fan1/state";
+const char *fan2_topic = "rackfanleft/fan2/state";
 #endif
 #ifdef RIGHT
-  const char *client_id = "rackfanright";
-  const char *fan1_status_topic = "rackfanright/fan1/status";
-  const char *fan2_status_topic = "rackfanright/fan2/status";
-  const char *fan1_topic = "rackfanright/fan1/state";
-  const char *fan2_topic = "rackfanright/fan2/state";
+const char *client_id = "rackfanright";
+const char *status_topic = "rackfanright/status";
+const char *fan1_status_topic = "rackfanright/fan1/status";
+const char *fan2_status_topic = "rackfanright/fan2/status";
+const char *fan1_topic = "rackfanright/fan1/state";
+const char *fan2_topic = "rackfanright/fan2/state";
 #endif
 #ifdef CENTER
-  const char *client_id = "rackfancenter";
-  const char *fan1_status_topic = "rackfancenter/fan1/status";
-  const char *fan2_status_topic = "rackfancenter/fan2/status";
-  const char *fan1_topic = "rackfancenter/fan1/state";
-  const char *fan2_topic = "rackfancenter/fan2/state";
+const char *client_id = "rackfancenter";
+const char *status_topic = "rackfancenter/status";
+const char *fan1_status_topic = "rackfancenter/fan1/status";
+const char *fan2_status_topic = "rackfancenter/fan2/status";
+const char *fan1_topic = "rackfancenter/fan1/state";
+const char *fan2_topic = "rackfancenter/fan2/state";
 #endif
-
-
 
 int fan1_status = 0;
 int fan2_status = 0;
@@ -49,6 +50,7 @@ unsigned long previousMillis = 0;
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
+  client.publish(status_topic, "receiving");
   unsigned int i = 0;
   for (; i < length; i++)
   {
@@ -88,7 +90,10 @@ void reconnect()
     {
       Serial.println("MQTT broker connected");
       client.publish(status_topic, "connected");
-      client.publish(motion_topic, "0", true);
+      client.publish(fan1_topic, "1");
+      client.publish(fan2_topic, "1");
+      client.subscribe(fan1_topic);
+      client.subscribe(fan2_topic);
     }
     else
     {
@@ -128,27 +133,28 @@ void loop()
     if (currentMillis - previousMillis >= 5000)
     {
       previousMillis = currentMillis;
+      client.loop();
       if (fan1_status == 0)
-  {
-    digitalWrite(fan1, LOW);
-    client.publish(fan1_status_topic, "off");
-  }
-  else
-  {
-    digitalWrite(fan1, HIGH);
-    client.publish(fan1_status_topic, "on");
-  }
+      {
+        digitalWrite(fan1, LOW);
+        client.publish(fan1_status_topic, "off");
+      }
+      else
+      {
+        digitalWrite(fan1, HIGH);
+        client.publish(fan1_status_topic, "on");
+      }
 
-  if (fan2_status == 0)
-  {
-    digitalWrite(fan2, LOW);
-    client.publish(fan2_status_topic, "off");
-  }
-  else
-  {
-    digitalWrite(fan2, HIGH);
-    client.publish(fan2_status_topic, "on");
-  }
+      if (fan2_status == 0)
+      {
+        digitalWrite(fan2, LOW);
+        client.publish(fan2_status_topic, "off");
+      }
+      else
+      {
+        digitalWrite(fan2, HIGH);
+        client.publish(fan2_status_topic, "on");
+      }
     }
   }
   else
